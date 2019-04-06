8574616fa235f05ab157e991aac1380428e15f79
jie-he-acct-buan <49351579+jie-he-acct-buan@users.noreply.github.com>
2019-04-06 17:50:11 -0500

    Add files via upload

---
+++ Cleaning Data in Python.py
@@ -0,0 +1,378 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 22 23:35:27 2019
+
+@author: Jie
+"""
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+###############################################################################
+# Import pandas
+import pandas as pd
+
+# Read the file into a DataFrame: df
+df = pd.read_csv('F:/0 - PhD at UTD/2019 Spring/DataCamp/Cleaning Data in Python/dob_job_application_filings_subset.csv')
+
+# Print the head of df
+print(df.head())
+
+# Print the tail of df
+print(df.tail())
+
+# Print the shape of df
+print(df.shape)
+
+# Print the columns of df
+print(df.columns)
+
+
+df_subset=df[['Job #', 'Doc #', 'Borough', 'Initial Cost', 'Total Est. Fee', 'Existing Zoning Sqft', 
+              'Proposed Zoning Sqft', 'Enlargement SQ Footage', 'Street Frontage', 'ExistingNo. of Stories',
+              'Proposed No. of Stories', 'Existing Height', 'Proposed Height']]
+# Print the head and tail of df_subset
+print(df_subset.head())
+print(df_subset.tail())
+###############################################################################
+# Print the info of df
+print(df.info())
+
+# Print the info of df_subset
+print(df_subset.info())
+
+###############################################################################
+# Print the value counts for 'Borough'
+print(df['Borough'].value_counts(dropna=False))
+
+# Print the value_counts for 'State'
+print(df['State'].value_counts(dropna=False))
+
+# Print the value counts for 'Site Fill'
+print(df['Site Fill'].value_counts(dropna=False))
+
+###############################################################################
+df['Existing Zoning Sqft'].describe()
+
+# Plot the histogram
+df['Existing Zoning Sqft'].plot(kind='hist', rot=70, logx=True, logy=True)
+
+# Display the histogram
+plt.show()
+###############################################################################
+# Create the boxplot
+df.boxplot(column='Initial Cost', by='Borough', rot=90)
+
+# Display the plot
+plt.show()
+###############################################################################
+airquality=pd.read_csv('F:/0 - PhD at UTD/2019 Spring/DataCamp/Cleaning Data in Python/airquality.csv')
+# Print the head of airquality
+print(airquality.head())
+
+# Melt airquality: airquality_melt
+airquality_melt = pd.melt(airquality, id_vars=['Month', 'Day'])
+
+# Print the head of airquality_melt
+print(airquality_melt.head())
+
+###############################################################################
+# Print the head of airquality
+print(airquality.head())
+
+# Melt airquality: airquality_melt
+airquality_melt = pd.melt(airquality, id_vars=['Month', 'Day'], var_name='measurement', value_name='reading')
+
+# Print the head of airquality_melt
+print(airquality_melt.head())
+###############################################################################
+# Print the head of airquality_melt
+print(airquality_melt.head())
+
+# Pivot airquality_melt: airquality_pivot
+airquality_pivot = airquality_melt.pivot_table(index=['Month', 'Day'], columns='measurement', values='reading')
+
+# Print the head of airquality_pivot
+print(airquality_pivot.head())
+###############################################################################
+# Print the index of airquality_pivot
+print(airquality_pivot.index)
+
+# Reset the index of airquality_pivot: airquality_pivot_reset
+airquality_pivot_reset = airquality_pivot.reset_index(['Month', 'Day'])
+
+# Print the new index of airquality_pivot_reset
+print(airquality_pivot_reset.index)
+
+# Print the head of airquality_pivot_reset
+print(airquality_pivot_reset.head())
+###############################################################################
+# Pivot table the airquality_dup: airquality_pivot
+airquality_pivot = airquality_dup.pivot_table(index=['Month', 'Day'], columns='measurement', values='reading', aggfunc=np.mean)
+
+# Print the head of airquality_pivot before reset_index
+print(airquality_pivot.head())
+
+# Reset the index of airquality_pivot
+airquality_pivot = airquality_pivot.reset_index(['Month','Day'])
+
+# Print the head of airquality_pivot
+print(airquality_pivot.head())
+
+# Print the head of airquality
+print(airquality.head())
+###############################################################################
+tb=pd.read_csv('F:/0 - PhD at UTD/2019 Spring/DataCamp/Cleaning Data in Python/tb.csv')
+print(tb.head())
+
+# Melt tb: tb_melt
+tb_melt = pd.melt(tb, id_vars=['country', 'year'])
+
+# Create the 'gender' column
+tb_melt['gender'] = tb_melt.variable.str[0]
+
+# Create the 'age_group' column
+tb_melt['age_group'] = tb_melt.variable.str[1:]
+
+# Print the head of tb_melt
+print(tb_melt.head())
+
+###############################################################################
+ebola=pd.read_csv('F:/0 - PhD at UTD/2019 Spring/DataCamp/Cleaning Data in Python/ebola.csv')
+# Melt ebola: ebola_melt
+ebola_melt = pd.melt(ebola, id_vars=['Date', 'Day'], var_name='type_country', value_name='counts')
+
+# Create the 'str_split' column
+ebola_melt['str_split'] = ebola_melt['type_country'].str.split('_')
+# Create the 'type' column
+ebola_melt['type'] = ebola_melt['str_split'].str.get(0)
+
+# Create the 'country' column
+ebola_melt['country'] = ebola_melt['str_split'].str.get(1)
+
+# Print the head of ebola_melt
+print(ebola_melt.head())
+
+###############################################################################
+# Concatenate ebola_melt and status_country column-wise: ebola_tidy
+ebola_tidy = pd.concat((ebola_melt, status_country), axis=1)
+
+# Print the shape of ebola_tidy
+print(ebola_tidy.shape)
+
+# Print the head of ebola_tidy
+print(ebola_tidy.head())
+
+###############################################################################
+# Import necessary modules
+import pandas as pd
+import glob
+
+# Write the pattern: pattern
+pattern = '*.csv'
+
+# Save all file matches: csv_files
+csv_files = glob.glob(pattern)
+
+# Print the file names
+print(csv_files)
+
+# Load the second file into a DataFrame: csv2
+csv2 = pd.read_csv(csv_files[1])
+
+# Print the head of csv2
+print(csv2.head())
+
+###############################################################################
+# Create an empty list: frames
+frames = []
+
+#  Iterate over csv_files
+for csv in csv_files:
+
+    #  Read csv into a DataFrame: df
+    df = pd.read_csv(csv)
+    
+    # Append df to frames
+    frames.append(df)
+    
+# Concatenate frames into a single DataFrame: uber
+uber = pd.concat(frames)
+
+# Print the shape of uber
+print(uber.shape)
+
+# Print the head of uber
+print(uber.head())
+
+###############################################################################
+# Merge the DataFrames: o2o
+o2o = pd.merge(left=site, right=visited, left_on='name', right_on='site')
+
+# Print o2o
+print(o2o)
+###############################################################################
+tips=pd.read_csv('F:/0 - PhD at UTD/2019 Spring/DataCamp/Cleaning Data in Python/tips.csv')
+tips['sex']=tips['sex'].astype('category')
+tips['smoker']=tips['smoker'].astype('category')
+tips['tip']=tips['tip'].astype(str)
+tips['tip']=pd.to_numeric(tips['tip'], errors='coerce')
+###############################################################################
+# Import the regular expression module
+import re
+
+# Compile the pattern: prog
+prog = re.compile('\d{3}-\d{3}-\d{4}')
+
+# See if the pattern matches
+result = prog.match('123-456-7890')
+print(bool(result))
+
+# See if the pattern matches
+result2 = prog.match('1123-456-7890')
+print(bool(result2))
+###############################################################################
+# Import the regular expression module
+import re
+
+# Find the numeric values: matches
+matches = re.findall('\d+', 'the recipe calls for 10 strawberries and 1 banana')
+
+# Print the matches
+print(matches)
+
+###############################################################################
+# Write the first pattern
+pattern1 = bool(re.match(pattern='\d{3}-\d{3}-\d{4}', string='123-456-7890'))
+print(pattern1)
+
+# Write the second pattern
+pattern2 = bool(re.match(pattern='\$\d*\.\d{2}', string='$123.45'))
+print(pattern2)
+
+# Write the third pattern
+pattern3 = bool(re.match(pattern='[A-Z]\w*', string='Australia'))
+print(pattern3)
+###############################################################################
+# Define recode_gender()
+def recode_gender(gender):
+
+    # Return 0 if gender is 'Female'
+    if gender == 'Female':
+        return 0
+    
+    # Return 1 if gender is 'Male'    
+    elif gender == 'Male':
+        return 1
+    
+    # Return np.nan    
+    else:
+        return np.nan
+
+# Apply the function to the sex column
+tips['recode'] = tips['sex'].apply(recode_gender)
+
+# Print the first five rows of tips
+print(tips.head())
+###############################################################################
+tips=pd.read_csv('F:/0 - PhD at UTD/2019 Spring/DataCamp/Cleaning Data in Python/tips.csv')
+
+# Write the lambda function using replace
+tips['total_dollar_replace'] = tips.total_dollar.apply(lambda x: x.replace('$', ''))
+
+# Write the lambda function using regular expressions
+tips['total_dollar_re'] = tips.total_dollar.apply(lambda x: re.findall('\d+\.\d+', x)[0])
+
+# Print the head of tips
+print(tips.head())
+
+###############################################################################
+g1800s=pd.read_csv('F:/0 - PhD at UTD/2019 Spring/DataCamp/Cleaning Data in Python/g1800s.csv')
+# Import matplotlib.pyplot
+import matplotlib.pyplot as plt
+
+# Create the scatter plot
+g1800s.plot(kind='scatter', x='1800', y='1899')
+
+# Specify axis labels
+plt.xlabel('Life Expectancy by Country in 1800')
+plt.ylabel('Life Expectancy by Country in 1899')
+
+# Specify axis limits
+plt.xlim(20, 55)
+plt.ylim(20, 55)
+
+# Display the plot
+plt.show()
+
+
+###############################################################################
+def check_null_or_valid(row_data):
+    """Function that takes a row of data,
+    drops all missing values,
+    and checks if all remaining values are greater than or equal to 0
+    """
+    no_na = row_data.dropna()
+    numeric = pd.to_numeric(no_na)
+    ge0 = numeric >= 0
+    return ge0
+
+# Check whether the first column is 'Life expectancy'
+assert g1800s.columns[100] == 'Life expectancy'
+
+# Check whether the values in the row are valid
+assert g1800s.iloc[:, :100].apply(check_null_or_valid, axis=1).all().all()
+
+# Check that there is only one instance of each country
+assert g1800s['Life expectancy'].value_counts()[0] == 1
+###############################################################################
+
+###############################################################################
+
+###############################################################################
+
+###############################################################################
+
+###############################################################################
+
+###############################################################################
+
+###############################################################################
+
+###############################################################################
+
+###############################################################################
+
+###############################################################################
+
+###############################################################################
+
+###############################################################################
+
+###############################################################################
+
+###############################################################################
+
+###############################################################################
+
+###############################################################################
+
+###############################################################################
+
+###############################################################################
+
+###############################################################################
+
+###############################################################################
+
+###############################################################################
+
+###############################################################################
+
+###############################################################################
+
+###############################################################################
+
+
